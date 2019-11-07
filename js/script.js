06ca8ed53dc78d242329e4209bacf363aa06c4a3
LorenzoCao21 <Lcao@hudsonhs.com>
2019-11-07 23:25:04 +0000

    apparently we did stuff

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+    $("button").click(function(){
+    let input = $(".input").val();
+//  let splitinput = input.split("")
+    let charinput = input.charAt(0);
+    let length = input.length;
 
-
-
-  
-  
-  
-  
+    let sliceinput = input.slice(1, length);
+    console.log(sliceinput + charinput + "ay")
+    let result = sliceinput + charinput + "ay"
+  //  let addition = (charinput)
+    $(".output").text(result);
+    });
 });
+
